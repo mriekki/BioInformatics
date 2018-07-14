4f6f2f784e37cf4bf156507a75047e3b84ff0ca5
Mark Riekki <mark.riekki@gmail.com>
2018-07-14 05:50:47 -0500

    week 1 exercises

--- BioInformaticsConsoleApp/BioInformaticsConsoleApp/Program.cs
+++ BioInformaticsConsoleApp/BioInformaticsConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+
 
 namespace BioInformaticsConsoleApp
 {
@@ -7,13 +9,15 @@ namespace BioInformaticsConsoleApp
     {
         static void Main(string[] args)
         {
-            string inputFile = "D:\\Temp\\dataset_9_6.txt";
+            string inputFile = "c:\\Temp\\Vibrio_cholerae.txt";
 
             string[] fileText = MyReadFile(inputFile);
 
             // Minimum Skew
             if (fileText.Length == 1)
             {
+                ReverseComplement(fileText[0]);
+
                 Console.WriteLine(MinimumSkew(fileText[0]));
             }
 
@@ -30,6 +34,21 @@ namespace BioInformaticsConsoleApp
             }       */
 
 
+            if (fileText.Length == 2)
+            {
+                string str1 = fileText[0];
+                string str2 = fileText[1];
+                int nDistance = 0;
+
+                //              Int32.TryParse(str2, out nDistance);
+
+
+                //                FrequentWords(str1, nDistance);
+
+                PatternMatchIndexes(str1, str2);
+            }
+
+
             if (fileText.Length == 3)
             {
                 string str1 = fileText[0];
@@ -40,19 +59,123 @@ namespace BioInformaticsConsoleApp
                 Int32.TryParse(str3, out nDistance);
 
                 int n = ApproximatePatternCount(str1, str2, nDistance);
+            }
+        }
 
-                Console.WriteLine("ApproximatePatternCount {0}", n);
+        static public int PatternCount(string Text, string Pattern)
+        {
+            int Count = 0;
+            string substring = "";
 
+            for (int i = 0; i < Text.Length - Pattern.Length +1; i++)
+            {
+                substring = Text.Substring(i, Pattern.Length);
+                if (substring == Pattern)
+                    Count += 1;
             }
 
+            Console.WriteLine("PatternCount:  {0} = {1}", Pattern, Count);
 
-/*            foreach (string s in fileText)
+            return Count;
+        }
+
+        static public string FrequentWords(string Text, int k)
+        {
+            string FrequentPatterns = "";
+            List<int> Count = new List<int>();
+            List<string> Kmers = new List<string>();
+            int MaxCount = 0;
+            int localCount = 0;
+
+            string pattern = "";
+
+            for (int i = 0; i < Text.Length - k; i++)
             {
-                Console.WriteLine(s);
-            }       */
-          }
+                pattern = Text.Substring(i, k);
+ 
+                localCount = PatternCount(Text, pattern);
+                if (localCount > MaxCount)
+                    MaxCount = localCount;
 
-         private static string MinimumSkew(String strInput)
+                Count.Add(localCount);
+                Kmers.Add(pattern);
+            }
+
+            List<string> tmpKmers = new List<string>();
+
+            for (k = 0; k < Count.Count; k++)
+            {
+                if (Count[k] == MaxCount)
+                    tmpKmers.Add(Kmers[k]);
+            }
+
+            tmpKmers = RemoveDuplicates(tmpKmers);
+
+            foreach (string s in tmpKmers)
+            {
+                FrequentPatterns += s + " ";
+            }
+
+            Console.WriteLine("FrequentWords:  {0}", FrequentPatterns);
+
+            return FrequentPatterns;
+
+        }
+
+        static public string ReverseComplement(string input)
+        {
+            string output = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                if (input[i] == 'A')
+                    output += "T";
+                else if (input[i] == 'T')
+                    output += "A";
+                else if (input[i] == 'G')
+                    output += "C";
+                else if (input[i] == 'C')
+                    output += "G";
+                else
+                    Console.WriteLine("ReverseComplement:  Invalid Value");
+            }
+
+            Console.WriteLine("ReverseComplment:  {0}", output);
+            return output;
+        }
+
+        static public string PatternMatchIndexes(string pattern, string text)
+        {
+            string result = "";
+
+            for (int i = 0; i < text.Length - pattern.Length + 1; i++)
+            {
+                if (text.Substring(i, pattern.Length) == pattern)
+                    result += i.ToString() + " ";
+            }
+            Console.WriteLine("PatternMatchIndexes:  {0}", result);
+            return result;
+        }
+
+        static public List<string> RemoveDuplicates(List<string> inputList)
+        {
+            List<string> outputList = new List<string>();
+
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (i == 0)
+                    outputList.Add(inputList[i]);
+                else
+                {
+                    if (!outputList.Contains(inputList[i]))
+                        outputList.Add(inputList[i]);
+                }
+            }
+
+            return outputList;
+        }
+
+        private static string MinimumSkew(String strInput)
         {
             string strResult = "";
             int count = 0;
